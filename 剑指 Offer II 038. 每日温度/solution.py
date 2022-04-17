fb365b35f14e1538f27f8fb687dcb4a087558059
LiuYu0521 <ly819933137@gmail.com>
2022-04-17 18:24:53 +0800

    单调栈第二种思路框架

--- 剑指 Offer II 038. 每日温度/solution.py
+++ 剑指 Offer II 038. 每日温度/solution.py
@@ -26,17 +26,30 @@
 from typing import List
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # stack = []
+        # res = [0]*len(temperatures)
+        # for i in range(len(temperatures)):
+        #     while len(stack) != 0 and temperatures[i] > temperatures[stack[-1]]:
+        #         index = stack.pop()
+        #         res[index] = i - index
+        #     stack.append(i)
+        # return res
+
         stack = []
         res = [0]*len(temperatures)
-        for i in range(len(temperatures)):
-            while len(stack) != 0 and temperatures[i] > temperatures[stack[-1]]:
-                index = stack.pop()
-                res[index] = i - index
+        for i in range(len(temperatures)-1, -1, -1):
+            while stack and temperatures[i] >= temperatures[stack[-1]]:
+                stack.pop()
+            if not stack:
+                res[i] = 0
+            else:
+                res[i] = stack[-1] - i
             stack.append(i)
         return res
+            
 
 
 if __name__ == '__main__':
     temperatures = [73,74,75,71,69,72,76,73]
     solution = Solution()
-    print(solution.dailyTemperatures(temperatures))
+    solution.dailyTemperatures(temperatures)
